33f351de81f6f47b65e5e527dc4c73899554c213
Mila-Kostic <milakostic.eng@gmail.com>
2022-05-21 11:14:48 +0200

    slider show

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 //alert("Happy codinig!");
-var x = document.querySelectorAll("src");
-console.log("X" + x);
+//var x = document.querySelectorAll("src");
+// console.log("X" + x);
+
+let img = document.querySelector('.img');
+        let counter = 0;
+        let loop = setInterval(function () {
+            counter++
+            if (counter === 4) {
+                counter = 1;
+            }
+            img.setAttribute('src','img/slika'+counter+'.jpg');
+        },2000)
